544c6e079413bb852f83227b03236afba368da5f
tony-montemuro <tonyamontemuro@gmail.com>
2024-02-06 13:47:33 -0600

    improved functionality

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
+const PREV_YEAR = 2023;
 const calendar = document.getElementById("calendar");
-calendar.max = new Date().toISOString().split("T")[0];  // current date
+calendar.max = `${PREV_YEAR}-12-31`;  // last day of previous year
 
 const form = document.querySelector("form");
 form.addEventListener("submit", calculateInflation);
 
+async function readLocalStorage(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, result => {
+            if (key in result) {
+                resolve(result[key]);
+            } else {
+                reject();
+            }
+        });
+    });
+};
+
+async function getCpi(month, year) {
+    try {
+        const months = await readLocalStorage(year.toString());
+        return parseFloat(months[month] ? months[month] : months.at(-1)); 
+    } catch (error) {
+        return undefined;
+    }
+};
+
 async function calculateInflation(e) {
     e.preventDefault();
     
-    const usd = document.getElementById("dollars").value;
+    const usd = parseFloat(document.getElementById("dollars").value);
     const date = calendar.value;
-    const output = document.getElementById("output");
+    const [year, month] = date.split("-").map(n => parseInt(n));
 
-    console.log(usd, date);
+    const cpiCurrent = await getCpi(12, PREV_YEAR);
+    const cpiOld = await getCpi(month, year);
+
+    const output = document.getElementById("output");
+    const error = document.getElementById("error");
+    if (typeof cpiCurrent !== "undefined" && typeof cpiOld !== "undefined"){
+        const adjusted = (cpiCurrent/cpiOld) * usd;
+        output.innerText = `$${adjusted.toFixed(2)}`;
+        error.innerText = "";
+    } else {
+        output.innerText = "";
+        error.innerText = "There was a problem during the calculation process. If the problem persists, reinstall the extension.";
+    }
 };
