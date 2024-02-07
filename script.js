0f90d508d002a98753c73c1399193e79e9e83203
tony-montemuro <tonyamontemuro@gmail.com>
2024-02-07 08:51:59 -0600

    added background image to extension

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const PREV_YEAR = 2023;
 const calendar = document.getElementById("calendar");
-calendar.max = `${PREV_YEAR}-12`;  // last day of previous year
 
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
